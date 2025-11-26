462015f715032b9aa1622ecc7586392ffe18467f
V Dinesh Chandra <VDinesh.Chandra@encora.com>
2025-11-26 17:57:47 +0530

    Strugle

---
+++ DoctorRegistration/Model/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DoctorRegistration.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Doctor> Doctors { get; set; }
+    }
+}   
